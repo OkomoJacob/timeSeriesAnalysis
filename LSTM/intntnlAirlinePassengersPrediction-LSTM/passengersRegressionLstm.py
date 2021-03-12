d9cc019158776e0cb4f25edfe52390ffa1ca3893
okomojacob <jaylabs2030@gmail.com>
2021-03-13 00:45:49 +0300

    New python Supper Regressor, LSTM

---
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
@@ -0,0 +1,11 @@
+
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from matplotlib import pyplot as plt
+from keras.layers import Dense
+from keras.layers import LSTM
+import pandas
+import math
+import numpy
