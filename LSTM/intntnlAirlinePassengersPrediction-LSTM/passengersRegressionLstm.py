49760a40a3853157e4de2d2305e7b1bb3b7bbe34
okomojacob <jaylabs2030@gmail.com>
2021-03-13 12:30:28 +0300

    further Code update, make predictions

--- LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
@@ -94,3 +94,20 @@ trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
+
+"""
+STEP 7: Generate Predictions
+"""
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
